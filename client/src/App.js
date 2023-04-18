6b0de700ea3167bd22e88e890498e99596c490c0
Sophie <89915926+SophieLindstrom@users.noreply.github.com>
2023-04-18 11:48:44 +0200

    added css changes

--- client/src/App.js
+++ client/src/App.js
@@ -11,26 +11,27 @@ export default function App() {
     if (!formValid) {
       return;
     }
-    setMessages(`Hello and welcome to Apendo, ${firstName} ${lastName} !`);
+    setMessages(`Hello and welcome to Apendo, ${firstName} ${lastName}!`);
   };
   return (
     <div>
+      <h1>Welcome to Apendo!</h1>
       <form onSubmit={submit}>
         <div>
-          <label >Enter first name</label>
+          <label className='label'>Enter first name</label>
           <input className='input'
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
           />
         </div>
         <div>
-          <label>Enter last name</label>
+          <label className='label'>Enter last name</label>
           <input className='input'
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
           />
         </div>
-        <button type="submit">submit</button>
+        <button className='label' type="submit">Submit</button>
       </form>
       {message}
     </div>
